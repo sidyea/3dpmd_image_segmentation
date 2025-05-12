dcb4ac00fcd718edbfa3d78266ac29049912421e
sidyea <115950981+sidyea@users.noreply.github.com>
2025-05-12 16:01:00 +0200

    implemented intensity metrics

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,10 @@
 
 # ROI crop variables
 
-roi_x_start = 0     # Starting x-coordinate (from left side)
-roi_x_end = 1000    # Ending x-coordinate (from left side)
-roi_y_start = 0     # Starting y-coordinate (from top)
-roi_y_end = 480     # Ending y-coordinate (from top)
+roi_x_start = 200     # Starting x-coordinate (from left side)
+roi_x_end = 800    # Ending x-coordinate (from left side)
+roi_y_start = 100   # Starting y-coordinate (from top)
+roi_y_end = 550     # Ending y-coordinate (from top)
 
 
 # Slice variables
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # imports
 from utils import video_loader
-from processing.preprocessing import crop_to_roi
-import os
+from processing.preprocessing import crop_to_roi, calculate_intensity_metrics
 import cv2
 import numpy as np
 
@@ -9,17 +8,32 @@ import numpy as np
 def main():
     print("Now running main.py")
 
+    results = []
+
+    # open source
     video_path = "C:/Users/shark/VSCodeProjects/FSCM/data/video/sideview/20250122-142515671393.webm" 
 
-    for frame in video_loader.load_video(video_path):
-        # Process the frame
-        # Example processing: crop to a region of interest (ROI)
-        frame = crop_to_roi(frame)
+    for i, frame in enumerate(video_loader.load_video(video_path)):
+        # Crop / Transform
+        frame = crop_to_roi(frame)      # crop (values in settings.py)
+        frame = frame.astype("float32")/255.0     # global normalization to [0, 1]
+
+        # Intensity metrics
+        metrics = calculate_intensity_metrics(frame)
+        
+        # save results
+
+
 
         # Show the frame
+        # Draw metrics on the frame
+        cv2.putText(frame, f"Mean: {metrics['mean']:.3f}", (400, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
         cv2.imshow("Frame", frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        
     
     # Clean up
     for i in range(1, 5):
